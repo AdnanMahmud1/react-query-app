8371b4aa9954c863a1804a1b58e27633da9bda1e
Adnan <Adnan@DESKTOP-UGPNVH0>
2023-05-09 14:06:35 +0600

    form submit tested

--- starter/src/Items.jsx
+++ starter/src/Items.jsx
@@ -1,9 +1,30 @@
-import SingleItem from './SingleItem';
+import SingleItem from "./SingleItem";
+import { useQuery } from "@tanstack/react-query";
+import customFetch from "./utils";
 
-const Items = ({ items }) => {
+const Items = () => {
+  const { isLoading, data, error, isError } = useQuery({
+    queryKey: ["tasks"],
+    queryFn: async () => {
+      const { data } = await customFetch.get("/");
+      return data;
+    },
+  });
+  console.log(data);
+  if (isLoading) {
+    return <p style={{ marginTop: "1rem" }}>Loading...</p>;
+  }
+  if(isError){
+    return <p style={{ marginTop: "1rem" }}>There was an error ...</p>;
+  }
+  //console.log(error);
+  if(error){
+    return <p style={{ marginTop: "1rem" }}>{error.message}</p>;
+  }
+
   return (
-    <div className='items'>
-      {items.map((item) => {
+    <div className="items">
+      {data.taskList.map((item) => {
         return <SingleItem key={item.id} item={item} />;
       })}
     </div>
--- starter/src/utils.js
+++ starter/src/utils.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const customFetch = () => {
-  baseUrl: "http://localhost:5000/api/tasks";
-};
+const customFetch = axios.create({
+  baseURL: "http://localhost:5000/api/tasks",
+});
 
 export default customFetch;
